7411e4480b0a2c8474b106805cc937889a3341ce
lake83 <lake83@mail.ru>
2019-04-28 00:54:13 +0300

    news

--- views/news/view.php
+++ views/news/view.php
@@ -1,8 +1,25 @@
 <?php
 
-use app\components\SiteHelper;
-
 /* @var $this yii\web\View */
 /* @var $model app\models\News */
 
+$this->title = $model->title;
+$this->registerMetaTag(['name' => 'keywords', 'content' => $model->keywords]);
+$this->registerMetaTag(['name' => 'description', 'content' => $model->description]);
 ?>
+<div class="content col-xs-12">
+    <?= \app\components\TopWidget::widget() ?>
+            
+    <div class="container">
+        <div class="row">
+            <div class="main-content col-xs-12 col-md-9">
+            
+            <?= $model->full_text ?>
+            
+            </div>
+                    
+            <?= \app\components\AsideWidget::widget() ?>
+                    
+        </div>
+    </div>
+</div>
