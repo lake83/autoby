96dc727d29710e709e480796dc108cbfc9512a3c
lake83 <lake83@mail.ru>
2019-05-10 22:04:45 +0300

    show btn

--- views/widgets/filter.php
+++ views/widgets/filter.php
@@ -269,7 +269,7 @@ if ($request->get('generation')) {
             <div class="btn-wrapper">
                 <?php if ($ads_count): ?>
                                     
-                <?= Html::submitButton(Yii::t('app', 'Показать <span>{n, plural, =0{#</span> предложений} =1{#</span> предложене} one{#</span> предложене} few{#</span> предложения} many{#</span> предложений} other{#</span> предложений}}', ['n' => $ads_count]), ['class' => 'blue-btn hidden-xs transition']) ?>
+                <?= Html::submitButton(Yii::t('app', 'Показать <span>{n, plural, =0{#</span> предложений} =1{#</span> предложене} one{#</span> предложене} few{#</span> предложения} many{#</span> предложений} other{#</span> предложений}}', ['n' => $ads_count]), ['class' => 'blue-btn transition']) ?>
                                     
                 <?php else: ?>
                                     
